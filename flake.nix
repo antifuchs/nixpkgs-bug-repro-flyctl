e9604682e91778c5b10a503aea29781b3e6a7540
Andreas Fuchs <asf@boinkor.net>
2023-05-25 11:40:51 -0400

    Build a FHS user env to run flyctl inside
    This doesn't trigger the bug on the CLI either, so that's weird.

--- flake.nix
+++ flake.nix
@@ -17,10 +17,21 @@
       version = "0.1.12";
       src = flyctl-0-1-12;
     });
-  in {
 
+    userenv = {pkgs, flyctl}: pkgs.buildFHSUserEnv {
+      name = "run-flyctl";
+      targetPkgs = (pkgs: with pkgs; [flyctl]) ;
+      runScript = pkgs.writeShellScript "run-flyctl" ''
+        exec flyctl "$@"
+      '';
+    };
+  in {
     apps.x86_64-linux.stable = {program = "${pkgs-stable.flyctl}/bin/flyctl"; type = "app";};
     apps.x86_64-linux.unstable = { program = "${pkgs-unstable.flyctl}/bin/flyctl"; type = "app";};
     apps.x86_64-linux.latest = { program = "${patched-flyctl}/bin/flyctl"; type = "app";};
-  };
+
+    apps.x86_64-linux.userenv-stable = {program = "${(userenv {pkgs = pkgs-stable; flyctl = pkgs-stable.flyctl;})}/bin/run-flyctl"; type = "app";};
+    apps.x86_64-linux.userenv-unstable = { program = "${(userenv {pkgs = pkgs-unstable; flyctl = pkgs-unstable.flyctl;})}/bin/run-flyctl"; type = "app";};
+    apps.x86_64-linux.userenv-latest = { program = "${(userenv {flyctl = patched-flyctl; pkgs = pkgs-unstable;})}/bin/run-flyctl"; type = "app";};
+ };
 }
